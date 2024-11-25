b188c9cb4b6cf1383be7d9c424d85b555d99b364
Bayram Gürbüz <120523897+BayramGurbuz@users.noreply.github.com>
2024-11-25 20:41:14 +0300

    Update home.js firebase configuration chagned

--- front-master/js/home.js
+++ front-master/js/home.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebas
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAZLKtvxoKis67l3tSchewo_Bl6kVBr0tk",
-    authDomain: "edushare-2839e.firebaseapp.com",
-    projectId: "edushare-2839e",
-    storageBucket: "edushare-2839e.firebasestorage.app",
-    messagingSenderId: "88830203425",
-    appId: "1:88830203425:web:3e1c3a5dea246292e2f560"
+    apiKey: "....",
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: ""...",",
+    messagingSenderId: ""...",",
+    appId: ""...","
   };
 
 // Initialize Firebase
